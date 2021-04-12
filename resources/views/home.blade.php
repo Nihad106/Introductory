8077f0008d96c403416be747b953877c1c2101f7
Md Nasim Hossain Nihad <1505106.mnh@ugrad.cse.buet.ac.bd>
2021-04-12 18:47:08 +0600

    Initial commit before merging with the latest version of Laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,77 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    @extends('layouts.master')
 
+
+
+
+
+@section('title')
+Registered Roles
+@endsection
+
+@section('content')
+<div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                <h4 class="card-title"> Simple Table</h4>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive">
+                  <table class="table">
+                    <thead class=" text-primary">
+                      <th>
+                        Name
+                      </th>
+                      <th>
+                        To Do List
+                      </th>
+                      <th>
+                        Email
+                      </th>
+                      <th>
+                        Usertype
+                      </th>
+                      <th>
+                        Approval
+                      </th>
+                      <th>
+                        Edit
+                      </th>
+
+                      <th>
+                      Delete
+                      </th>
+                      
+                    </thead>
+                    
+                    <tbody>
+                      
+                     
+                        
+                     
+                        
+                    </tbody>
+                        
+                    
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+
+                
+        
+        </div>
+@endsection
+
+
+
+@section('scripts')
+
+@endsection
                     {{ __('You are logged in!') }}
                 </div>
             </div>
